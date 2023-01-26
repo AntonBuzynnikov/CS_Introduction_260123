42bbd47236aa5bdc88f4694f2c6d64f62d0805c3
Buzynnikov <buzynnikov2111@gmail.com>
2023-01-27 01:30:17 +0700

    Edited the project 2.0

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -2,3 +2,10 @@
 System.Console.Write("Введите число: ");
 int a = Int32.Parse(System.Console.ReadLine());
 System.Console.WriteLine("Квадрат числа = "+a*a);
+/*Решение с семинара
+string? s;
+s = Console.ReadLine();
+int a;
+a = Convert.ToInt32(s);
+Console.WriteLine(a*a);
+*/
