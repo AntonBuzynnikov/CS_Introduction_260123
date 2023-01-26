45557dbb93329d3205dcb5c68cddf3b40de06e51
Buzynnikov <buzynnikov2111@gmail.com>
2023-01-27 01:52:24 +0700

    Complete example_002

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// С клавиатуры вводятся 2 вещественных числа.Проверить является ли одно из них квадратом другого
+System.Console.WriteLine("Введите первое число: ");
+double a = Double.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+double b = Double.Parse(System.Console.ReadLine());
+if (a*a == b)
+    System.Console.WriteLine(b + " является квадратом "+ a);
+if (b*b == a)
+    System.Console.WriteLine(a+" является квадратом "+b);
