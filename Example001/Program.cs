554d4a5f7246bebc864ce50906e2bbb7b299cd50
Buzynnikov <buzynnikov2111@gmail.com>
2023-01-27 01:02:30 +0700

    Complete example

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿// 1. С клавиатуры вводится целое число. Вывести квадрат числа
+System.Console.Write("Введите число: ");
+string a = System.Console.ReadLine();
+int b = Int32.Parse(a);
+System.Console.WriteLine("Квадрат числа = "+b*b);
