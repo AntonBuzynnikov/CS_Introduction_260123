a3537493b8b821a7f61dafa47069d7bcb31788ed
Buzynnikov <buzynnikov2111@gmail.com>
2023-01-28 07:22:00 +0700

    Complete example006

---
+++ Example006/Program.cs
@@ -0,0 +1,4 @@
+﻿// 6. Написать программу вычисления значения функции y = sin(a). (Класс Math).
+System.Console.Write("Введите число: ");
+double x = Double.Parse(Console.ReadLine());
+System.Console.WriteLine("Sin(x) = "+ Math.Sin(x));
