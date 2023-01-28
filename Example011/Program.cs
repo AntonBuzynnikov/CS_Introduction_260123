c488404f48ebdcb52500e0194fb91de3c6272883
Buzynnikov <buzynnikov2111@gmail.com>
2023-01-28 08:28:54 +0700

    Complete example011

---
+++ Example011/Program.cs
@@ -0,0 +1,9 @@
+﻿// 11. Дано число больше 9, вывести второе число с конца
+System.Console.Write("Введите число: ");
+int a = Int32.Parse(System.Console.ReadLine());
+int b;
+
+b = a % 100;
+
+System.Console.Write("Второе число с конца: ");
+System.Console.Write(b / 10);
