505eb12fd44a149643108230698bcc0cd15e2240
Buzynnikov <buzynnikov2111@gmail.com>
2023-01-28 07:56:13 +0700

    Complete example007

---
+++ Example007/Program.cs
@@ -0,0 +1,7 @@
+﻿// 7. Выяснить является ли число чётным.
+System.Console.Write("Введите число: ");
+int a = Int32.Parse(System.Console.ReadLine());
+if (a % 2 == 0)
+    System.Console.WriteLine("Число чётное");
+else
+    System.Console.WriteLine("Число нечётное");
