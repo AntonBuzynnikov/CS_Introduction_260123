2531b1331fc12a281ea576a5ab965ea45c5c1121
Buzynnikov <buzynnikov2111@gmail.com>
2023-01-28 08:02:06 +0700

    Complete example008

---
+++ Example008/Program.cs
@@ -0,0 +1,10 @@
+﻿// 8. Вывести на экран числа от -N до N
+System.Console.Write("Введите число: ");
+int N  = Int32.Parse(System.Console.ReadLine());
+int i;
+i = -N;
+while (i <= N)
+{   
+    System.Console.Write(i+" ");
+    i++;
+}
