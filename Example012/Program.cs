72fc4b3464a19e2cf9912fd665f1f22ad9296db5
Buzynnikov <buzynnikov2111@gmail.com>
2023-01-28 08:34:29 +0700

    Complete example012

---
+++ Example012/Program.cs
@@ -0,0 +1,10 @@
+﻿// 12. С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+System.Console.Write("Введите число: ");
+int a = Int32.Parse(System.Console.ReadLine());
+int b,c;
+b = a / 10;
+c = a % 10;
+if (b > c)
+    System.Console.WriteLine(b);
+else
+    System.Console.WriteLine(c);
